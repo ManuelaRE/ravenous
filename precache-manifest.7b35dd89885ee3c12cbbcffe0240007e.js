2c387cdccf065240ca0838f10bf69b975ad794f9
Manuela Redinciuc <46057566+ManuelaRE@users.noreply.github.com>
2020-05-28 16:44:08 +0100

    Updates

---
+++ precache-manifest.7b35dd89885ee3c12cbbcffe0240007e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7795bc04cbab8e09794dc9817343c21",
+    "url": "/ManuelaRE/ravenous/index.html"
+  },
+  {
+    "revision": "e1c123ef9c119dc71908",
+    "url": "/ManuelaRE/ravenous/static/css/main.3c5769ca.chunk.css"
+  },
+  {
+    "revision": "10126e3e925687b0b41e",
+    "url": "/ManuelaRE/ravenous/static/js/2.b152fa3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ManuelaRE/ravenous/static/js/2.b152fa3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c123ef9c119dc71908",
+    "url": "/ManuelaRE/ravenous/static/js/main.fa67d944.chunk.js"
+  },
+  {
+    "revision": "1cecf80c04571713a3c1",
+    "url": "/ManuelaRE/ravenous/static/js/runtime-main.4c933d07.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ManuelaRE/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ManuelaRE/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
